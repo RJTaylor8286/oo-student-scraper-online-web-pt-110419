7ed931876d72b9c8f6135f730f0d9bbafc1a8bb6
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2020-01-14 16:03:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open()
+html = open(https://learn-co-curriculum.github.io/student-scraper-test-page/)
 class Scraper
 
   def self.scrape_index_page(index_url)
